b1a72f35428613baf46d0c1e1c70fc829c7b72d5
Lassi Ropponen <ropponenlassi@gmail.com>
2025-06-03 13:46:31 +0300

    add gson

---
+++ src/main/java/server/basic/CurrencyResponse.java
@@ -0,0 +1,8 @@
+package server.basic;
+
+import java.util.Map;
+
+public class CurrencyResponse {
+    public String date;
+    public Map<String, Double> eur;
+}
--- src/main/java/server/basic/Server.java
+++ src/main/java/server/basic/Server.java
@@ -5,6 +5,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.net.URI;
+import com.google.gson.Gson;
 
 public class Server {
     public static void main(String[] args) {
@@ -12,9 +13,13 @@ public class Server {
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"))
             .build();
-        client.sendAsync(request, BodyHandlers.ofString())
+        String response = client.sendAsync(request, BodyHandlers.ofString())
             .thenApply(HttpResponse::body)
-            .thenAccept(System.out::println)
-            .join(); 
+            .join();
+        System.out.println(response);
+
+        Gson gson = new Gson();
+        CurrencyResponse responseObj = gson.fromJson(response, CurrencyResponse.class);
+        System.out.println(responseObj.eur.get("usd"));
     }
 }
